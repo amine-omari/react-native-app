69884071241560f6876a4fe603748a87c3166ee4
amine alaoui <omari1amineco@gmail.com>
2024-05-20 12:25:14 +0100

    feat: add props to ScreenHeaderBtn component

--- components/common/header/ScreenHeaderBtn.tsx
+++ components/common/header/ScreenHeaderBtn.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Image, TouchableOpacity } from "react-native";
+import styles from "./screenheader.style";
 
 type TScreenHeaderBtn = {
   iconUrl: string;
   dimension: string;
-  handlePress: any;
+  handlePress?: any;
 };
 
 const ScreenHeaderBtn = ({
@@ -13,8 +14,17 @@ const ScreenHeaderBtn = ({
   handlePress,
 }: TScreenHeaderBtn) => {
   return (
-    <TouchableOpacity>
-      <Image src={iconUrl} />
+    <TouchableOpacity
+      // @ts-ignore
+      style={styles.btnContainer}
+      onPress={handlePress}
+    >
+      <Image
+        src={iconUrl}
+        resizeMode="cover"
+        // @ts-ignore
+        style={styles.btnImg(dimension)}
+      />
     </TouchableOpacity>
   );
 };
