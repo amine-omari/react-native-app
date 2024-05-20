bee19d66ae28a34aac77b3043b389254d27223e6
amine alaoui <omari1amineco@gmail.com>
2024-05-20 08:13:48 +0100

    feat: add useRouter hook to Home file

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
+import { useRouter } from "expo-router";
 import React from "react";
 import { SafeAreaView, Text, View } from "react-native";
 
 const Home = () => {
+  const router = useRouter();
+
   return (
     <SafeAreaView>
       <Text>Home</Text>
