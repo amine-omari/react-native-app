5288b0c3013d35fe7a4c89fccc1962a89440521c
amine alaoui <omari1amineco@gmail.com>
2024-05-25 12:52:50 +0100

    feat: add headerBtn style

--- components/home/popular/Popularjobs.tsx
+++ components/home/popular/Popularjobs.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
 import styles from "./popularjobs.style";
 
@@ -8,6 +8,9 @@ const Popularjobs = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
