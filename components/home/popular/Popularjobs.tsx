f84eef1a7e0f8eefe676f0176608034a5deefb57
amine alaoui <omari1amineco@gmail.com>
2024-05-25 13:40:02 +0100

    feat: import ActivityIndicator to PopularJobs component

--- components/home/popular/Popularjobs.tsx
+++ components/home/popular/Popularjobs.tsx
@@ -1,9 +1,12 @@
 import React from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
 import styles from "./popularjobs.style";
+import { COLORS } from "@/constants";
 
 const Popularjobs = () => {
+  const isLoading = false;
+  const error = false;
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -12,7 +15,15 @@ const Popularjobs = () => {
           <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.cardsContainer}></View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" color={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          <Text></Text>
+        )}
+      </View>
     </View>
   );
 };
