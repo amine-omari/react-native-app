e07f220eb470402d8d51a4e98129dfd9438b10e7
amine omari <omari1amineco@gmail.com>
2025-02-02 22:36:54 +0300

    feat: add SafeAreaView style

--- app/index.js
+++ app/index.js
@@ -1,14 +1,15 @@
 import { View, ScrollView, SafeAreaView, Text } from "react-native";
 import { useState } from "react";
 import { useRouter } from "expo-router";
+import { COLORS } from "../constants";
 
 const Home = () => {
   const router = useRouter();
 
   return (
-    <View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
       <Text></Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
