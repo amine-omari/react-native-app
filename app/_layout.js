5362181b4363f9a23d5b40e6dc680263c0ee1d5c
amine omari <omari1amineco@gmail.com>
2025-02-03 10:09:33 +0300

    feat: add fonts

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,16 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
+  const [fontLoaded] = useFonts({
+    DBBold: require("../assets/fonts/DMSans-Bold.ttf"),
+    DBMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+    DBRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+  });
+
   return <Stack />;
 };
 
