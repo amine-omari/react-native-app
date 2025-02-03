522732f8ab5695c65a86aaa0b029ad1b3b7efea1
amine omari <omari1amineco@gmail.com>
2025-02-03 09:29:09 +0300

    feat: add useRouter

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import { View, Text } from "react-native";
 
 import styles from "./welcome.style";
+import { useRouter } from "expo-router";
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
       <Text>Welcome</Text>
