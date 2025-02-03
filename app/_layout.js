79b8e8fab617c094bf3e49733c75a072b3dd8901
amine omari <omari1amineco@gmail.com>
2025-02-03 10:11:03 +0300

    feat: add onLayoutRoot if statement

--- app/_layout.js
+++ app/_layout.js
@@ -1,15 +1,22 @@
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
+import { useCallback } from "react";
 
 SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-  const [fontLoaded] = useFonts({
+  const [fontsLoaded] = useFonts({
     DBBold: require("../assets/fonts/DMSans-Bold.ttf"),
     DBMedium: require("../assets/fonts/DMSans-Medium.ttf"),
     DBRegular: require("../assets/fonts/DMSans-Regular.ttf"),
   });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
 
   return <Stack />;
 };
