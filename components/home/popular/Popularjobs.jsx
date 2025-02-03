0654f358bd3380a53fae7395ad9a8f31000543ef
amine omari <omari1amineco@gmail.com>
2025-02-03 11:29:01 +0300

    feat: add useRouter

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
+  const router = useRouter();
+
   return (
     <View>
       <Text>Popularjobs</Text>
