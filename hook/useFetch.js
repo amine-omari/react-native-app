047f7368715e60829b95d669b26b34553f5946cc
amine omari <omari1amineco@gmail.com>
2025-02-03 12:27:19 +0300

    feat: add error state

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -3,4 +3,5 @@ import { useState } from "react";
 const useFetch = () => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 };
