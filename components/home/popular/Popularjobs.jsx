e021f82fa522c3839ef9dba6cfd365a645ea326c
amine omari <omari1amineco@gmail.com>
2025-02-03 12:00:38 +0300

    add a button

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 import { useRouter } from "expo-router";
 
 import styles from "./popularjobs.style";
@@ -11,6 +11,9 @@ const Popularjobs = () => {
     <View style={styles.container}>
       <View style={styles.header}>
         <Text style={styles.headerTitle}>Popular jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
