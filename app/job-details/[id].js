9cfcdb3609e0d0a0a87dcaf79ec2162a68c2f04e
amine omari <omari1amineco@gmail.com>
2025-02-04 11:38:46 +0300

    feat: pass all the props for Company component

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -61,7 +61,12 @@ const JobDetails = () => {
             <Text>No Data</Text>
           ) : (
             <View style={{ padding: SIZES.medium, paddingBottom: 1000 }}>
-              <Company />
+              <Company
+                companyLogo={data[0].employer_logo}
+                jobTitle={data[0].job_title}
+                companyName={data[0].employer_name}
+                Location={data[0].job_country}
+              />
 
               <JobTabs />
             </View>
