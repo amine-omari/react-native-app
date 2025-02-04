b726f7862d801c1b3e5b56d6b8b7c9fe2d7568f5
amine omari <omari1amineco@gmail.com>
2025-02-04 12:04:49 +0300

    feat: add log image style

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -14,6 +14,7 @@ const Company = (companyLogo, jobTitle, companyName, Location) => {
               ? companyLogo
               : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
           }}
+          style={styles.logoImage}
         />
       </View>
     </View>
