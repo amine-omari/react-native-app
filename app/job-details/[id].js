2a5a019157ba22c2be653819afd4c1cbf9984a3b
amine omari <omari1amineco@gmail.com>
2025-02-04 10:33:30 +0300

    feat: import useFetch hook

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,10 +1,15 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { useRouter, useSearchParams } from "expo-router";
+import useFetch from "../../hook/useFetch";
 
 const JobDetails = () => {
   const params = useSearchParams();
   const router = useRouter();
+
+  const { data, isLoading, error, refetch } = useFetch("job-details", {
+    job_id: params.id,
+  });
 
   return (
     <View>
