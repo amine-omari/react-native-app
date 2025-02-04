688c3bb1640add0a4743dc31e9016601864367b5
amine omari <omari1amineco@gmail.com>
2025-02-04 12:04:12 +0300

    feat: add companyLogo image

--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,12 +1,21 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 
 import styles from "./company.style";
+import { checkImageURL } from "../../../utils";
 
 const Company = (companyLogo, jobTitle, companyName, Location) => {
   return (
     <View style={styles.container}>
-      <View style={styles.logoBox}></View>
+      <View style={styles.logoBox}>
+        <Image
+          source={{
+            uri: checkImageURL(companyLogo)
+              ? companyLogo
+              : "https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg",
+          }}
+        />
+      </View>
     </View>
   );
 };
