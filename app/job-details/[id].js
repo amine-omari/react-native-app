5afc76133d82ccc7474f26c0554e8fe03f541718
amine omari <omari1amineco@gmail.com>
2025-02-04 10:30:47 +0300

    feat: import useRouter hook from expo router

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,9 +1,11 @@
 import { View, Text } from "react-native";
 import React from "react";
-import { useSearchParams } from "expo-router/build/hooks";
+import { useRouter, useSearchParams } from "expo-router";
 
 const JobDetails = () => {
   const params = useSearchParams();
+  const router = useRouter();
+
   return (
     <View>
       <Text>JobDetails</Text>
