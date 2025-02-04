4d16680be9232de769f3261ae3e9dcf666f0ce4e
amine omari <omari1amineco@gmail.com>
2025-02-04 10:29:02 +0300

    feat: import useSearchParams hook

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,7 +1,9 @@
 import { View, Text } from "react-native";
 import React from "react";
+import { useSearchParams } from "expo-router/build/hooks";
 
 const JobDetails = () => {
+  const params = useSearchParams();
   return (
     <View>
       <Text>JobDetails</Text>
